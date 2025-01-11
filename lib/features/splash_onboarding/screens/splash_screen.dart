dc0f139496d2305ee05bb68ce466405302d74e3e
Salma Marey <salmamarey2001@gmail.com>
2025-01-11 19:31:44 +0200

    check login

--- lib/features/splash_onboarding/screens/splash_screen.dart
+++ lib/features/splash_onboarding/screens/splash_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:e_commerce_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:e_commerce_app/core/models/user_model.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +15,30 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    await Future.delayed(const Duration(seconds: 0));
+    final isLoggedIn = await _isUserLoggedIn();
+    if (isLoggedIn) {
+      Navigator.pushReplacementNamed(context, '/layout');
+    } else {
+      Navigator.pushReplacementNamed(context, '/start');
+    }
+  }
+
+  Future<bool> _isUserLoggedIn() async {
+    final userBox = Hive.box<UserModel>('userBox');
+    return userBox.isNotEmpty;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(child: Image.asset(Assets.logoApp)),
+    );
   }
 }
