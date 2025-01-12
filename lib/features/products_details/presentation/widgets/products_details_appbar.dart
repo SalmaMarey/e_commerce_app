05aa49e4f9aa36ab3f76a8ed582035e44e9732d5
Salma Marey <salmamarey2001@gmail.com>
2025-01-12 03:41:06 +0200

    edit

--- lib/features/products_details/presentation/widgets/products_details_appbar.dart
+++ lib/features/products_details/presentation/widgets/products_details_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_app/core/themes/app_colors.dart';
 import 'package:e_commerce_app/core/themes/app_text_styles.dart';
-import 'package:e_commerce_app/features/home/presentation/widgets/fav_icon.dart';
+import 'package:e_commerce_app/features/favorite/presentation/widgets/fav_icon.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
