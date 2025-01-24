4def35ecd02abd6a21953554b775efc230928170
Salma Marey <salmamarey2001@gmail.com>
2025-01-24 03:59:45 +0200

    edit cart to go to product screen

--- lib/features/cart/presentation/screens/cart_screen.dart
+++ lib/features/cart/presentation/screens/cart_screen.dart
@@ -1,10 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce_app/core/models/cart_model.dart';
+import 'package:e_commerce_app/core/services/di.dart';
 import 'package:e_commerce_app/core/themes/app_colors.dart';
 import 'package:e_commerce_app/core/themes/app_text_styles.dart';
 import 'package:e_commerce_app/features/cart/presentation/controller/cart_bloc.dart';
 import 'package:e_commerce_app/features/cart/presentation/controller/cart_event.dart';
 import 'package:e_commerce_app/features/cart/presentation/controller/cart_state.dart';
+import 'package:e_commerce_app/features/products_details/domain/usecases/check_favorite_usecase.dart';
+import 'package:e_commerce_app/features/products_details/domain/usecases/get_products_details_usecase.dart';
+import 'package:e_commerce_app/features/products_details/domain/usecases/toggle_favorite_usecase.dart';
+import 'package:e_commerce_app/features/products_details/presentation/controller/products_details_bloc.dart';
+import 'package:e_commerce_app/features/products_details/presentation/screens/products_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -59,81 +65,106 @@ class _CartScreenState extends State<CartScreen> {
                 itemCount: cartItems.length,
                 itemBuilder: (context, index) {
                   final cartItem = cartItems[index];
-                  return Card(
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BlocProvider(
+                            create: (context) => ProductDetailsBloc(
+                              getProductDetailsUseCase:
+                                  di<GetProductDetailsUseCase>(),
+                              toggleFavoriteUseCase: di<ToggleFavoriteUseCase>(
+                                  param1: widget.userId),
+                              checkFavoriteUseCase: di<CheckFavoriteUseCase>(
+                                  param1: widget.userId),
+                            ),
+                            child: ProductDetailsScreen(
+                              productId: cartItem.productId,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    child: Card(
                       elevation: 4,
                       margin: const EdgeInsets.symmetric(
                           horizontal: 16, vertical: 8),
                       child: ListTile(
-                          leading: CachedNetworkImage(
-                            imageUrl: cartItem.imageUrl,
-                            width: 60,
-                            height: 60,
-                            placeholder: (context, url) => const Center(
-                              child: CircularProgressIndicator(),
+                        leading: CachedNetworkImage(
+                          imageUrl: cartItem.imageUrl,
+                          width: 60,
+                          height: 60,
+                          placeholder: (context, url) => const Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                        title: Text(
+                          cartItem.productName,
+                          maxLines: 1,
+                          style: AppTextStyles.font16Bold.copyWith(
+                            color: AppColors.textColor,
+                          ),
+                        ),
+                        subtitle: Text(
+                          '\$${cartItem.price}',
+                          style: AppTextStyles.font14BoldBlack.copyWith(
+                            color: AppColors.primaryColor,
+                          ),
+                        ),
+                        trailing: SizedBox(
+                          width: 120.w,
+                          height: 30.h,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.r),
+                              border: Border.all(color: AppColors.primaryColor),
                             ),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                          title: Text(
-                            cartItem.productName,
-                            maxLines: 1,
-                            style: AppTextStyles.font16Bold
-                                .copyWith(color: AppColors.textColor),
-                          ),
-                          subtitle: Text(
-                            '\$${cartItem.price}',
-                            style: AppTextStyles.font14BoldBlack
-                                .copyWith(color: AppColors.primaryColor),
-                          ),
-                          trailing: SizedBox(
-                              width: 120.w,
-                              height: 30.h,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15.r),
-                                  border:
-                                      Border.all(color: AppColors.primaryColor),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(
+                                    Icons.remove,
+                                    color: AppColors.primaryColor,
+                                    size: 20,
+                                  ),
+                                  onPressed: () {
+                                    _updateQuantity(context, cartItem,
+                                        cartItem.quantity - 1);
+                                  },
+                                  padding: EdgeInsets.zero,
+                                  constraints: const BoxConstraints(),
                                 ),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.remove,
-                                        color: AppColors.primaryColor,
-                                        size: 20,
-                                      ),
-                                      onPressed: () {
-                                        _updateQuantity(context, cartItem,
-                                            cartItem.quantity - 1);
-                                      },
-                                      padding: EdgeInsets.zero,
-                                      constraints: const BoxConstraints(),
-                                    ),
-                                    Text(
-                                      '${cartItem.quantity}',
-                                      style: AppTextStyles.font16Bold.copyWith(
-                                        color: AppColors.textColor,
-                                      ),
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.add,
-                                        color: AppColors.primaryColor,
-                                        size: 20,
-                                      ),
-                                      onPressed: () {
-                                        _updateQuantity(context, cartItem,
-                                            cartItem.quantity + 1);
-                                      },
-                                      padding: EdgeInsets.zero,
-                                      constraints: const BoxConstraints(),
-                                    ),
-                                  ],
+                                Text(
+                                  '${cartItem.quantity}',
+                                  style: AppTextStyles.font16Bold.copyWith(
+                                    color: AppColors.textColor,
+                                  ),
                                 ),
-                              ))));
+                                IconButton(
+                                  icon: const Icon(
+                                    Icons.add,
+                                    color: AppColors.primaryColor,
+                                    size: 20,
+                                  ),
+                                  onPressed: () {
+                                    _updateQuantity(context, cartItem,
+                                        cartItem.quantity + 1);
+                                  },
+                                  padding: EdgeInsets.zero,
+                                  constraints: const BoxConstraints(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
                 },
               );
             }
